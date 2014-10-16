2d32440476d2a606b6057147c339b03bded10385
sillymoose <davidnmfarrell@gmail.com>
2014-10-15 21:36:10 -0400

    added radians and header_vector methods

--- lib/Math/Shape/Vector.pm
+++ lib/Math/Shape/Vector.pm
@@ -5,7 +5,7 @@ package Math::Shape::Vector;
 use 5.008;
 use Carp;
 use Math::Shape::Utils;
-use Math::Trig;
+use Math::Trig qw/acos :pi/;
 
 # ABSTRACT: A 2d vector library in cartesian space
 
@@ -26,13 +26,13 @@ use Math::Trig;
 
 =head1 DESCRIPTION
 
-This module contains 2d vector-based objects intended as base classes for 2d games programming. Most of the objects have collision detection (among other methods). The objects available are:
+This module contains 2d vector-based objects intended as base classes for 2d games programming. Most of the objects have collision detection (among other methods). All objects are immutable in so far as their methods return new objects everytime. The objects available are:
 
 =over
 
 =item *
 
-L<Math::Shape::Vector> - a 2d vector (this module)
+L<Math::Shape::Vector> - a 2d vector (this package)
 
 =item *
 
@@ -237,6 +237,11 @@ Returns the vector length.
 
     my $length = $vector->length;
 
+Useful if you want to calculate the distance between two vectors:
+
+    my $vector_c = $vector_a->subtract_vector($vector_b);
+    my $distance_a_to_b = $vector_c->length;
+
 =cut
 
 sub length {
@@ -250,7 +255,7 @@ sub length {
 
 =head2 convert_to_unit_vector
 
-Returns a new vector object with a length of 1.
+Returns a new vector object with a length of 1 (aka a normalized vector).
 
     my $unit_vector = $vector->convert_to_unit_vector;
 
@@ -331,6 +336,35 @@ sub enclosed_angle
     acos( $ua->dot_product($ub) );
 }
 
+=head2 radians
+
+Returns the angle of the vector expressed in radians (0 - 2 pi).
+
+    $vector->radians;
+
+=cut
+
+sub radians
+{
+    my $radians = atan2 $_[0]->{x}, $_[0]->{y};
+    # if less than zero
+    $radians < 0 ? pi2 + $radians : $radians;
+}
+
+=head2 header_vector
+
+Returns a unit (normalized) vector representing the direction towards another vector. This header_vector can be converted into radians using the C<radians> method. Requires an L<Math::Shape::Vector> object as an argument.
+
+    my $header_vector = $vector_a->header_vector($vector_b);
+    my $radians_a_to_b = $header_vector->radians;
+
+=cut
+
+sub header_vector
+{
+    croak 'must pass a vector object' unless $_[1]->isa('Math::Shape::Vector');
+    $_[1]->subtract_vector($_[0])->convert_to_unit_vector;
+}
 
 =head2 collides
 
